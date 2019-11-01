b4165aad2db97639373164da4141bb7e995a7d63
Andy Pan <panjf2000@gmail.com>
2019-11-01 14:44:27 +0800

    Add prototypes of encoders/decoders

--- codec/codec.go
+++ codec/codec.go
@@ -8,3 +8,15 @@ type ICodec interface {
 	Encode(in, out []byte, packet gopacket.Packet) error
 	Decode(buf []byte, packet gopacket.Packet) ([]byte, error)
 }
+
+type LineBasedFrameDecoder struct {
+}
+
+type DelimiterBasedFrameDecoder struct {
+}
+
+type FixedLengthFrameDecoder struct {
+}
+
+type LengthFieldBasedFrameDecoder struct {
+}
