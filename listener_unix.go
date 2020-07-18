0c1bcb3bcaa026d1b2796cb69e47c09c65c32bb7
Andy Pan <panjf2000@gmail.com>
2020-07-18 21:47:02 +0800

    trifle: update some copyright statements

--- listener_unix.go
+++ listener_unix.go
@@ -1,5 +1,4 @@
 // Copyright (c) 2019 Andy Pan
-// Copyright (c) 2018 Joshua J Baker
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
