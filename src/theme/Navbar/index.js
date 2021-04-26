231d78b355ec779e0c574dfa0aee5db1218120bc
Andy Pan <panjf2000@gmail.com>
2021-04-27 00:13:35 +0800

    Add Github stars

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -33,7 +33,7 @@ function navLinkAttributes(label, right) {
       return attrs;
 
     case 'github':
-      attrs.badge = '4.1k';
+      attrs.badge = '4.2k';
       attrs.hideText = false;
       attrs.icon = 'github';
       return attrs;
