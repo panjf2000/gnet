538c99c271d0e68b24559fba76d73fa0dfd8dfd0
Andy Pan <panjf2000@gmail.com>
2021-05-10 21:42:36 +0800

    Update github stars

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -33,7 +33,7 @@ function navLinkAttributes(label, right) {
       return attrs;
 
     case 'github':
-      attrs.badge = '4.2k';
+      attrs.badge = '4.3k';
       attrs.hideText = false;
       attrs.icon = 'github';
       return attrs;
