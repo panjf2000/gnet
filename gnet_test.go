43466f60d7e71db12b4a807a6aba861f60786294
zhongweikang <zhongweikang@sfmail.sf-express.com>
2021-11-20 19:45:38 +0800

    test: add test for so_reuseaddr

--- gnet_test.go
+++ gnet_test.go
@@ -48,108 +48,128 @@ func TestCodecServe(t *testing.T) {
 	t.Run("poll", func(t *testing.T) {
 		t.Run("tcp", func(t *testing.T) {
 			t.Run("1-loop-LineBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9991", false, false, 10, false, new(LineBasedFrameCodec))
+				testCodecServe(t, "tcp", ":9991", false, false, 10, false, false, new(LineBasedFrameCodec))
 			})
 			t.Run("1-loop-DelimiterBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9992", false, false, 10, false, NewDelimiterBasedFrameCodec('|'))
+				testCodecServe(t, "tcp", ":9992", false, false, 10, false, false, NewDelimiterBasedFrameCodec('|'))
 			})
 			t.Run("1-loop-FixedLengthFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9993", false, false, 10, false, NewFixedLengthFrameCodec(64))
+				testCodecServe(t, "tcp", ":9993", false, false, 10, false, false, NewFixedLengthFrameCodec(64))
 			})
 			t.Run("1-loop-LengthFieldBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9994", false, false, 10, false, nil)
+				testCodecServe(t, "tcp", ":9994", false, false, 10, false, false, nil)
 			})
 			t.Run("N-loop-LineBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9995", true, false, 10, false, new(LineBasedFrameCodec))
+				testCodecServe(t, "tcp", ":9995", true, false, 10, false, false, new(LineBasedFrameCodec))
 			})
 			t.Run("N-loop-DelimiterBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9996", true, false, 10, false, NewDelimiterBasedFrameCodec('|'))
+				testCodecServe(t, "tcp", ":9996", true, false, 10, false, false, NewDelimiterBasedFrameCodec('|'))
 			})
 			t.Run("N-loop-FixedLengthFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9997", true, false, 10, false, NewFixedLengthFrameCodec(64))
+				testCodecServe(t, "tcp", ":9997", true, false, 10, false, false, NewFixedLengthFrameCodec(64))
 			})
 			t.Run("N-loop-LengthFieldBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9998", true, false, 10, false, nil)
+				testCodecServe(t, "tcp", ":9998", true, false, 10, false, false, nil)
 			})
 		})
 		t.Run("tcp-async", func(t *testing.T) {
 			t.Run("1-loop-LineBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9991", false, true, 10, false, new(LineBasedFrameCodec))
+				testCodecServe(t, "tcp", ":9991", false, true, 10, false, false, new(LineBasedFrameCodec))
 			})
 			t.Run("1-loop-DelimiterBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9992", false, true, 10, false, NewDelimiterBasedFrameCodec('|'))
+				testCodecServe(t, "tcp", ":9992", false, true, 10, false, false, NewDelimiterBasedFrameCodec('|'))
 			})
 			t.Run("1-loop-FixedLengthFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9993", false, true, 10, false, NewFixedLengthFrameCodec(64))
+				testCodecServe(t, "tcp", ":9993", false, true, 10, false, false, NewFixedLengthFrameCodec(64))
 			})
 			t.Run("1-loop-LengthFieldBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9994", false, true, 10, false, nil)
+				testCodecServe(t, "tcp", ":9994", false, true, 10, false, false, nil)
 			})
 			t.Run("N-loop-LineBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9995", true, true, 10, false, new(LineBasedFrameCodec))
+				testCodecServe(t, "tcp", ":9995", true, true, 10, false, false, new(LineBasedFrameCodec))
 			})
 			t.Run("N-loop-DelimiterBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9996", true, true, 10, false, NewDelimiterBasedFrameCodec('|'))
+				testCodecServe(t, "tcp", ":9996", true, true, 10, false, false, NewDelimiterBasedFrameCodec('|'))
 			})
 			t.Run("N-loop-FixedLengthFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9997", true, true, 10, false, NewFixedLengthFrameCodec(64))
+				testCodecServe(t, "tcp", ":9997", true, true, 10, false, false, NewFixedLengthFrameCodec(64))
 			})
 			t.Run("N-loop-LengthFieldBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9998", true, true, 10, false, nil)
+				testCodecServe(t, "tcp", ":9998", true, true, 10, false, false, nil)
 			})
 		})
 	})
+
 	t.Run("poll-reuseport", func(t *testing.T) {
 		t.Run("tcp", func(t *testing.T) {
 			t.Run("1-loop-LineBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9991", false, false, 10, true, new(LineBasedFrameCodec))
+				testCodecServe(t, "tcp", ":9991", false, false, 10, true, true, new(LineBasedFrameCodec))
 			})
 			t.Run("1-loop-DelimiterBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9992", false, false, 10, true, NewDelimiterBasedFrameCodec('|'))
+				testCodecServe(t, "tcp", ":9992", false, false, 10, true, true, NewDelimiterBasedFrameCodec('|'))
 			})
 			t.Run("1-loop-FixedLengthFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9993", false, false, 10, true, NewFixedLengthFrameCodec(64))
+				testCodecServe(t, "tcp", ":9993", false, false, 10, true, true, NewFixedLengthFrameCodec(64))
 			})
 			t.Run("1-loop-LengthFieldBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9994", false, false, 10, true, nil)
+				testCodecServe(t, "tcp", ":9994", false, false, 10, true, true, nil)
 			})
 			t.Run("N-loop-LineBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9995", true, false, 10, true, new(LineBasedFrameCodec))
+				testCodecServe(t, "tcp", ":9995", true, false, 10, true, true, new(LineBasedFrameCodec))
 			})
 			t.Run("N-loop-DelimiterBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9996", true, false, 10, true, NewDelimiterBasedFrameCodec('|'))
+				testCodecServe(t, "tcp", ":9996", true, false, 10, true, true, NewDelimiterBasedFrameCodec('|'))
 			})
 			t.Run("N-loop-FixedLengthFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9997", true, false, 10, true, NewFixedLengthFrameCodec(64))
+				testCodecServe(t, "tcp", ":9997", true, false, 10, true, true, NewFixedLengthFrameCodec(64))
 			})
 			t.Run("N-loop-LengthFieldBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9998", true, false, 10, true, nil)
+				testCodecServe(t, "tcp", ":9998", true, false, 10, true, true, nil)
 			})
 		})
 		t.Run("tcp-async", func(t *testing.T) {
 			t.Run("1-loop-LineBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9991", false, true, 10, true, new(LineBasedFrameCodec))
+				testCodecServe(t, "tcp", ":9991", false, true, 10, true, true, new(LineBasedFrameCodec))
 			})
 			t.Run("1-loop-DelimiterBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9992", false, true, 10, true, NewDelimiterBasedFrameCodec('|'))
+				testCodecServe(t, "tcp", ":9992", false, true, 10, true, true, NewDelimiterBasedFrameCodec('|'))
 			})
 			t.Run("1-loop-FixedLengthFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9993", false, true, 10, true, NewFixedLengthFrameCodec(64))
+				testCodecServe(t, "tcp", ":9993", false, true, 10, true, true, NewFixedLengthFrameCodec(64))
 			})
 			t.Run("1-loop-LengthFieldBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9994", false, true, 10, true, nil)
+				testCodecServe(t, "tcp", ":9994", false, true, 10, true, true, nil)
 			})
 			t.Run("N-loop-LineBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9995", true, true, 10, true, new(LineBasedFrameCodec))
+				testCodecServe(t, "tcp", ":9995", true, true, 10, true, true, new(LineBasedFrameCodec))
 			})
 			t.Run("N-loop-DelimiterBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9996", true, true, 10, true, NewDelimiterBasedFrameCodec('|'))
+				testCodecServe(t, "tcp", ":9996", true, true, 10, true, true, NewDelimiterBasedFrameCodec('|'))
 			})
 			t.Run("N-loop-FixedLengthFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9997", true, true, 10, true, NewFixedLengthFrameCodec(64))
+				testCodecServe(t, "tcp", ":9997", true, true, 10, true, true, NewFixedLengthFrameCodec(64))
 			})
 			t.Run("N-loop-LengthFieldBasedFrameCodec", func(t *testing.T) {
-				testCodecServe(t, "tcp", ":9998", true, true, 10, true, nil)
+				testCodecServe(t, "tcp", ":9998", true, true, 10, true, true, nil)
+			})
+		})
+	})
+
+	t.Run("poll-reuseaddr", func(t *testing.T) {
+		t.Run("tcp", func(t *testing.T) {
+			t.Run("1-loop-LineBasedFrameCodec", func(t *testing.T) {
+				testCodecServe(t, "tcp", ":9991", false, false, 10, false, true, new(LineBasedFrameCodec))
+			})
+			t.Run("N-loop-LineBasedFrameCodec", func(t *testing.T) {
+				testCodecServe(t, "tcp", ":9992", true, false, 10, false, true, new(LineBasedFrameCodec))
+			})
+		})
+		t.Run("tcp-async", func(t *testing.T) {
+			t.Run("1-loop-LineBasedFrameCodec", func(t *testing.T) {
+				testCodecServe(t, "tcp", ":9991", false, true, 10, false, true, new(LineBasedFrameCodec))
+			})
+			t.Run("N-loop-LineBasedFrameCodec", func(t *testing.T) {
+				testCodecServe(t, "tcp", ":9992", true, true, 10, false, true, new(LineBasedFrameCodec))
 			})
 		})
 	})
@@ -229,6 +249,7 @@ func testCodecServe(
 	multicore, async bool,
 	nclients int,
 	reuseport bool,
+	reuseaddr bool,
 	codec ICodec,
 ) {
 	var err error
@@ -270,6 +291,7 @@ func testCodecServe(
 		WithSocketSendBuffer(8*1024),
 		WithCodec(codec),
 		WithReusePort(reuseport),
+		WithReuseAddr(reuseaddr),
 	)
 	assert.NoError(t, err)
 }
@@ -321,50 +343,50 @@ func TestServe(t *testing.T) {
 	t.Run("poll", func(t *testing.T) {
 		t.Run("tcp", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "tcp", ":9991", false, false, false, 10, RoundRobin)
+				testServe(t, "tcp", ":9991", false, false, false, false, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "tcp", ":9992", false, true, false, 10, LeastConnections)
+				testServe(t, "tcp", ":9992", false, false, true, false, 10, LeastConnections)
 			})
 		})
 		t.Run("tcp-async", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "tcp", ":9991", false, false, true, 10, RoundRobin)
+				testServe(t, "tcp", ":9991", false, false, false, true, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "tcp", ":9992", false, true, true, 10, LeastConnections)
+				testServe(t, "tcp", ":9992", false, false, true, true, 10, LeastConnections)
 			})
 		})
 		t.Run("udp", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "udp", ":9991", false, false, false, 10, RoundRobin)
+				testServe(t, "udp", ":9991", false, false, false, false, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "udp", ":9992", false, true, false, 10, LeastConnections)
+				testServe(t, "udp", ":9992", false, false, true, false, 10, LeastConnections)
 			})
 		})
 		t.Run("udp-async", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "udp", ":9991", false, false, true, 10, RoundRobin)
+				testServe(t, "udp", ":9991", false, false, false, true, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "udp", ":9992", false, true, true, 10, LeastConnections)
+				testServe(t, "udp", ":9992", false, false, true, true, 10, LeastConnections)
 			})
 		})
 		t.Run("unix", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "unix", "gnet1.sock", false, false, false, 10, RoundRobin)
+				testServe(t, "unix", "gnet1.sock", false, false, false, false, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "unix", "gnet2.sock", false, true, false, 10, SourceAddrHash)
+				testServe(t, "unix", "gnet2.sock", false, false, true, false, 10, SourceAddrHash)
 			})
 		})
 		t.Run("unix-async", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "unix", "gnet1.sock", false, false, true, 10, RoundRobin)
+				testServe(t, "unix", "gnet1.sock", false, false, false, true, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "unix", "gnet2.sock", false, true, true, 10, SourceAddrHash)
+				testServe(t, "unix", "gnet2.sock", false, false, true, true, 10, SourceAddrHash)
 			})
 		})
 	})
@@ -372,50 +394,101 @@ func TestServe(t *testing.T) {
 	t.Run("poll-reuseport", func(t *testing.T) {
 		t.Run("tcp", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "tcp", ":9991", true, false, false, 10, RoundRobin)
+				testServe(t, "tcp", ":9991", true, true, false, false, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "tcp", ":9992", true, true, false, 10, LeastConnections)
+				testServe(t, "tcp", ":9992", true, true, true, false, 10, LeastConnections)
 			})
 		})
 		t.Run("tcp-async", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "tcp", ":9991", true, false, true, 10, RoundRobin)
+				testServe(t, "tcp", ":9991", true, true, false, true, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "tcp", ":9992", true, true, false, 10, LeastConnections)
+				testServe(t, "tcp", ":9992", true, true, true, false, 10, LeastConnections)
 			})
 		})
 		t.Run("udp", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "udp", ":9991", true, false, false, 10, RoundRobin)
+				testServe(t, "udp", ":9991", true, true, false, false, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "udp", ":9992", true, true, false, 10, LeastConnections)
+				testServe(t, "udp", ":9992", true, true, true, false, 10, LeastConnections)
 			})
 		})
 		t.Run("udp-async", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "udp", ":9991", true, false, false, 10, RoundRobin)
+				testServe(t, "udp", ":9991", true, true, false, false, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "udp", ":9992", true, true, true, 10, LeastConnections)
+				testServe(t, "udp", ":9992", true, true, true, true, 10, LeastConnections)
 			})
 		})
 		t.Run("unix", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "unix", "gnet1.sock", true, false, false, 10, RoundRobin)
+				testServe(t, "unix", "gnet1.sock", true, true, false, false, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "unix", "gnet2.sock", true, true, false, 10, LeastConnections)
+				testServe(t, "unix", "gnet2.sock", true, true, true, false, 10, LeastConnections)
 			})
 		})
 		t.Run("unix-async", func(t *testing.T) {
 			t.Run("1-loop", func(t *testing.T) {
-				testServe(t, "unix", "gnet1.sock", true, false, true, 10, RoundRobin)
+				testServe(t, "unix", "gnet1.sock", true, true, false, true, 10, RoundRobin)
 			})
 			t.Run("N-loop", func(t *testing.T) {
-				testServe(t, "unix", "gnet2.sock", true, true, true, 10, LeastConnections)
+				testServe(t, "unix", "gnet2.sock", true, true, true, true, 10, LeastConnections)
+			})
+		})
+	})
+
+	t.Run("poll-reuseaddr", func(t *testing.T) {
+		t.Run("tcp", func(t *testing.T) {
+			t.Run("1-loop", func(t *testing.T) {
+				testServe(t, "tcp", ":9991", false, true, false, false, 10, RoundRobin)
+			})
+			t.Run("N-loop", func(t *testing.T) {
+				testServe(t, "tcp", ":9992", false, true, true, false, 10, LeastConnections)
+			})
+		})
+		t.Run("tcp-async", func(t *testing.T) {
+			t.Run("1-loop", func(t *testing.T) {
+				testServe(t, "tcp", ":9991", false, true, false, true, 10, RoundRobin)
+			})
+			t.Run("N-loop", func(t *testing.T) {
+				testServe(t, "tcp", ":9992", false, true, true, false, 10, LeastConnections)
+			})
+		})
+		t.Run("udp", func(t *testing.T) {
+			t.Run("1-loop", func(t *testing.T) {
+				testServe(t, "udp", ":9991", false, true, false, false, 10, RoundRobin)
+			})
+			t.Run("N-loop", func(t *testing.T) {
+				testServe(t, "udp", ":9992", false, true, true, false, 10, LeastConnections)
+			})
+		})
+		t.Run("udp-async", func(t *testing.T) {
+			t.Run("1-loop", func(t *testing.T) {
+				testServe(t, "udp", ":9991", false, true, false, false, 10, RoundRobin)
+			})
+			t.Run("N-loop", func(t *testing.T) {
+				testServe(t, "udp", ":9992", false, true, true, true, 10, LeastConnections)
+			})
+		})
+		t.Run("unix", func(t *testing.T) {
+			t.Run("1-loop", func(t *testing.T) {
+				testServe(t, "unix", "gnet1.sock", false, true, false, false, 10, RoundRobin)
+			})
+			t.Run("N-loop", func(t *testing.T) {
+				testServe(t, "unix", "gnet2.sock", false, true, true, false, 10, LeastConnections)
+			})
+		})
+		t.Run("unix-async", func(t *testing.T) {
+			t.Run("1-loop", func(t *testing.T) {
+				testServe(t, "unix", "gnet1.sock", false, true, false, true, 10, RoundRobin)
+			})
+			t.Run("N-loop", func(t *testing.T) {
+				testServe(t, "unix", "gnet2.sock", false, true, true, true, 10, LeastConnections)
 			})
 		})
 	})
@@ -514,7 +587,7 @@ func (s *testServer) Tick() (delay time.Duration, action Action) {
 	return
 }
 
-func testServe(t *testing.T, network, addr string, reuseport, multicore, async bool, nclients int, lb LoadBalancing) {
+func testServe(t *testing.T, network, addr string, reuseport, reuseaddr, multicore, async bool, nclients int, lb LoadBalancing) {
 	ts := &testServer{
 		tester:     t,
 		network:    network,
@@ -529,6 +602,7 @@ func testServe(t *testing.T, network, addr string, reuseport, multicore, async b
 		WithLockOSThread(async),
 		WithMulticore(multicore),
 		WithReusePort(reuseport),
+		WithReuseAddr(reuseaddr),
 		WithTicker(true),
 		WithTCPKeepAlive(time.Minute*1),
 		WithTCPNoDelay(TCPDelay),
