1ab5f7f77c0f5bad27a70148244fb3f1858bdf34
Andy Pan <panjf2000@gmail.com>
2021-12-05 19:03:50 +0800

    Release gnet v1.6.0

--- metadata.js
+++ metadata.js
@@ -1,16 +1,16 @@
 module.exports = {
   "latest_highlight": {
     "author_github": "https://github.com/panjf2000",
-    "date": "2021-07-20",
-    "description": "Released v1.5.2",
+    "date": "2021-12-05",
+    "description": "Released v1.6.0",
     "hide_on_release_notes": false,
-    "id": "2021-07-20-released-1-5-2",
-    "path": "website/highlights/2021-07-20-released-1-5-2.md",
-    "permalink": "/highlights/2021-07-20-released-1-5-2/",
+    "id": "2021-12-05-released-1-6-0",
+    "path": "website/highlights/2021-12-05-released-1-6-0.md",
+    "permalink": "/highlights/2021-12-05-released-1-6-0/",
     "tags": [
       "release",
     ],
-    "title": "Released v1.5.2"
+    "title": "Released v1.6.0"
   },
   "latest_post": {
     "author_github": "https://github.com/panjf2000",
