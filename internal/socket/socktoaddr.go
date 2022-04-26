50406b34552ef6fb86795a9ee6c0e54ad71ae322
Andy Pan <panjf2000@gmail.com>
2022-04-26 20:00:58 +0800

    opt: put the bytes of net.Addr.Zone back to pool when connection is closed
    Fixes #364

--- internal/socket/socktoaddr.go
+++ internal/socket/socktoaddr.go
@@ -22,6 +22,7 @@ import (
 
 	"golang.org/x/sys/unix"
 
+	"github.com/panjf2000/gnet/v2/internal/toolkit"
 	bsPool "github.com/panjf2000/gnet/v2/pkg/pool/byteslice"
 )
 
@@ -100,5 +101,5 @@ func int2decimal(i uint) string {
 		bp--
 		b[bp] = byte(i%10) + '0'
 	}
-	return string(b[bp:])
+	return toolkit.BytesToString(b[bp:])
 }
