f2e748bcc06338e4e8039121bf4d395f6a05d60a
Andy Pan <panjf2000@gmail.com>
2022-08-09 23:38:08 +0800

    Bump up Github stars

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -33,7 +33,7 @@ function navLinkAttributes(label, right) {
       return attrs;
 
     case 'github':
-      attrs.badge = '6.7k';
+      attrs.badge = '6.8k';
       attrs.hideText = false;
       attrs.icon = 'github';
       return attrs;
