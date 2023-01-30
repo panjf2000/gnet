3f215222ce5a59ba2bdcef930cc98a7f58c2d655
0-haha <65914167+0-haha@users.noreply.github.com>
2023-01-30 20:28:04 +0000

    opt: replace syscall (deprecated golang library) with golang.org/x/sys/unix

--- pkg/tls/ktls_linux.go
+++ pkg/tls/ktls_linux.go
@@ -10,7 +10,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/unix"
@@ -63,14 +62,14 @@ func init() {
 		return
 	}
 	kTLSSupport = true && kTLSEnabled
-	Debugf("kTLS Enabled Status: %v\n", kTLSSupport)
+	Debugf("kTLS Enabled Status: %v", kTLSSupport)
 	// no need to check further, as KTLS is disabled
 	if !kTLSSupport {
 		return
 	}
 
-	var uname syscall.Utsname
-	if err := syscall.Uname(&uname); err != nil {
+	var uname unix.Utsname
+	if err := unix.Uname(&uname); err != nil {
 		Debugf("kTLS: call uname failed %v", err)
 		return
 	}
@@ -97,7 +96,7 @@ func init() {
 		return
 	}
 
-	Debugf("Kernel Version: %s\n", release)
+	Debugf("Kernel Version: %s", release)
 
 	if (major == 4 && minor >= 13) || major > 4 {
 		kTLSSupportTX = true
@@ -127,17 +126,17 @@ func init() {
 	}
 
 	Debugln("======Supported Features======")
-	Debugf("kTLS TX: %v\n", kTLSSupportTX)
-	Debugf("kTLS RX: %v\n", kTLSSupportRX)
-	Debugf("kTLS TLS 1.3 TX: %v\n", kTLSSupportTLS13TX)
-	Debugf("kTLS TLS 1.3 RX: %v\n", kTLSSupportTLS13RX)
-	Debugf("kTLS TX ZeroCopy: %v\n", kTLSSupportZEROCOPY)
-	Debugf("kTLS RX Expected No Pad: %v\n", kTLSSupportNOPAD)
+	Debugf("kTLS TX: %v", kTLSSupportTX)
+	Debugf("kTLS RX: %v", kTLSSupportRX)
+	Debugf("kTLS TLS 1.3 TX: %v", kTLSSupportTLS13TX)
+	Debugf("kTLS TLS 1.3 RX: %v", kTLSSupportTLS13RX)
+	Debugf("kTLS TX ZeroCopy: %v", kTLSSupportZEROCOPY)
+	Debugf("kTLS RX Expected No Pad: %v", kTLSSupportNOPAD)
 
 	Debugln("=========CipherSuites=========")
-	Debugf("kTLS AES-GCM-128: %v\n", kTLSSupportAESGCM128)
-	Debugf("kTLS AES-GCM-256: %v\n", kTLSSupportAESGCM256)
-	Debugf("kTLS CHACHA20POLY1305: %v\n", kTLSSupportCHACHA20POLY1305)
+	Debugf("kTLS AES-GCM-128: %v", kTLSSupportAESGCM128)
+	Debugf("kTLS AES-GCM-256: %v", kTLSSupportAESGCM256)
+	Debugf("kTLS CHACHA20POLY1305: %v", kTLSSupportCHACHA20POLY1305)
 }
 
 func (c *Conn) ReadFrom(r io.Reader) (n int64, err error) {
@@ -171,12 +170,12 @@ func (c *Conn) writeToFile(f *os.File, remain int64) (written int64, err error, 
 
 	// mmap must align on a page boundary
 	// mmap from 0, use data from offset
-	bytes, err := syscall.Mmap(int(f.Fd()), 0, int(offset+remain),
-		syscall.PROT_WRITE, syscall.MAP_SHARED)
+	bytes, err := unix.Mmap(int(f.Fd()), 0, int(offset+remain),
+		unix.PROT_WRITE, unix.MAP_SHARED)
 	if err != nil {
 		return 0, nil, false
 	}
-	defer syscall.Munmap(bytes)
+	defer unix.Munmap(bytes)
 
 	bytes = bytes[offset : offset+remain]
 	var (
@@ -245,7 +244,7 @@ func (c *Conn) spliceToFile(f *os.File, remain int64) (written int64, err error,
 			n, err = unix.Splice(int(rfd), nil, pwfd, nil, int(n), unix.SPLICE_F_MORE)
 			remain -= n
 			written += n
-			if err == syscall.EAGAIN {
+			if err == unix.EAGAIN {
 				// return false to wait data from connection
 				err = nil
 				return false
@@ -286,8 +285,8 @@ func (c *Conn) spliceToFile(f *os.File, remain int64) (written int64, err error,
 
 // destroyTempPipe destroys a temporary pipe.
 func destroyTempPipe(prfd, pwfd int) error {
-	err := syscall.Close(prfd)
-	err1 := syscall.Close(pwfd)
+	err := unix.Close(prfd)
+	err1 := unix.Close(pwfd)
 	if err == nil {
 		return err1
 	}
@@ -382,26 +381,25 @@ func (c *Conn) enableKernelTLS(cipherSuiteID uint16, inKey, outKey, inIV, outIV 
 	return nil
 }
 
-func ktlsReadRecord(fd int, b []byte) (recordType, int, error) {
+func ktlsReadRecord(fd int, b []byte) (typ recordType, n int, err error) {
 	// cmsg for record type
-	buffer := make([]byte, syscall.CmsgSpace(1))
-	cmsg := (*syscall.Cmsghdr)(unsafe.Pointer(&buffer[0]))
-	cmsg.SetLen(syscall.CmsgLen(1))
+	buffer := make([]byte, unix.CmsgSpace(1))
+	cmsg := (*unix.Cmsghdr)(unsafe.Pointer(&buffer[0]))
+	cmsg.SetLen(unix.CmsgLen(1))
 
-	var iov syscall.Iovec
+	var iov unix.Iovec
 	iov.Base = &b[0]
 	iov.SetLen(len(b))
 
-	var msg syscall.Msghdr
+	var msg unix.Msghdr
 	msg.Control = &buffer[0]
 	msg.Controllen = cmsg.Len
 	msg.Iov = &iov
 	msg.Iovlen = 1
 
-	var n int
 	flags := 0
-	n, err := recvmsg(uintptr(fd), &msg, flags)
-	if err == syscall.EAGAIN {
+	n, err = recvmsg(uintptr(fd), &msg, flags)
+	if err == unix.EAGAIN {
 		// data is not ready, goroutine will be parked
 		return 0, n, err
 	}
@@ -433,7 +431,7 @@ func ktlsReadRecord(fd int, b []byte) (recordType, int, error) {
 		Debugf("kTLS: unsupported cmsg type: %d", cmsg.Type)
 		return 0, 0, fmt.Errorf("unsupported cmsg type: %d", cmsg.Type)
 	}
-	typ := recordType(buffer[syscall.SizeofCmsghdr])
+	typ = recordType(buffer[unix.SizeofCmsghdr])
 	Debugf("kTLS: recvmsg, type: %d, payload len: %d", typ, n)
 	return typ, n, nil
 }
@@ -459,8 +457,8 @@ func ktlsReadDataFromRecord(fd int, b []byte) (int, error) {
 	}
 }
 
-func recvmsg(fd uintptr, msg *syscall.Msghdr, flags int) (n int, err error) {
-	r0, _, e1 := syscall.Syscall(syscall.SYS_RECVMSG, fd, uintptr(unsafe.Pointer(msg)), uintptr(flags))
+func recvmsg(fd uintptr, msg *unix.Msghdr, flags int) (n int, err error) {
+	r0, _, e1 := unix.Syscall(unix.SYS_RECVMSG, fd, uintptr(unsafe.Pointer(msg)), uintptr(flags))
 	n = int(r0)
 	if e1 != 0 {
 		err = errnoErr(e1)
@@ -468,8 +466,8 @@ func recvmsg(fd uintptr, msg *syscall.Msghdr, flags int) (n int, err error) {
 	return
 }
 
-func sendmsg(fd uintptr, msg *syscall.Msghdr, flags int) (n int, err error) {
-	r0, _, e1 := syscall.Syscall(syscall.SYS_SENDMSG, fd, uintptr(unsafe.Pointer(msg)), uintptr(flags))
+func sendmsg(fd uintptr, msg *unix.Msghdr, flags int) (n int, err error) {
+	r0, _, e1 := unix.Syscall(unix.SYS_SENDMSG, fd, uintptr(unsafe.Pointer(msg)), uintptr(flags))
 	n = int(r0)
 	if e1 != 0 {
 		err = errnoErr(e1)
@@ -480,22 +478,22 @@ func sendmsg(fd uintptr, msg *syscall.Msghdr, flags int) (n int, err error) {
 // Do the interface allocations only once for common
 // Errno values.
 var (
-	errEAGAIN error = syscall.EAGAIN
-	errEINVAL error = syscall.EINVAL
-	errENOENT error = syscall.ENOENT
+	errEAGAIN error = unix.EAGAIN
+	errEINVAL error = unix.EINVAL
+	errENOENT error = unix.ENOENT
 )
 
 // errnoErr returns common boxed Errno values, to prevent
 // allocations at runtime.
-func errnoErr(e syscall.Errno) error {
+func errnoErr(e unix.Errno) error {
 	switch e {
 	case 0:
 		return nil
-	case syscall.EAGAIN:
+	case unix.EAGAIN:
 		return errEAGAIN
-	case syscall.EINVAL:
+	case unix.EINVAL:
 		return errEINVAL
-	case syscall.ENOENT:
+	case unix.ENOENT:
 		return errENOENT
 	}
 	return e
@@ -503,18 +501,18 @@ func errnoErr(e syscall.Errno) error {
 
 func ktlsSendCtrlMessage(fd int, typ recordType, b []byte) (int, error) {
 	// cmsg for record type
-	buffer := make([]byte, syscall.CmsgSpace(1))
-	cmsg := (*syscall.Cmsghdr)(unsafe.Pointer(&buffer[0]))
-	cmsg.SetLen(syscall.CmsgLen(1))
-	buffer[syscall.SizeofCmsghdr] = byte(typ)
+	buffer := make([]byte, unix.CmsgSpace(1))
+	cmsg := (*unix.Cmsghdr)(unsafe.Pointer(&buffer[0]))
+	cmsg.SetLen(unix.CmsgLen(1))
+	buffer[unix.SizeofCmsghdr] = byte(typ)
 	cmsg.Level = SOL_TLS
 	cmsg.Type = TLS_SET_RECORD_TYPE
 
-	var iov syscall.Iovec
+	var iov unix.Iovec
 	iov.Base = &b[0]
 	iov.SetLen(len(b))
 
-	var msg syscall.Msghdr
+	var msg unix.Msghdr
 	msg.Control = &buffer[0]
 	msg.Controllen = cmsg.Len
 	msg.Iov = &iov
@@ -523,7 +521,7 @@ func ktlsSendCtrlMessage(fd int, typ recordType, b []byte) (int, error) {
 	var n int
 	flags := 0
 	n, err := sendmsg(uintptr(fd), &msg, flags)
-	if err == syscall.EAGAIN {
+	if err == unix.EAGAIN {
 		// data is not ready, goroutine will be parked
 		return n, err
 	}
