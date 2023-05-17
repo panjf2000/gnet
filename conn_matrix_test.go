0cdbe54498fa545c08b256f3843b5f5c72a5b0dc
Andy Pan <panjf2000@gmail.com>
2023-05-17 14:42:25 +0800

    test: fix the data race in TestConnMatrix

--- conn_matrix_test.go
+++ conn_matrix_test.go
@@ -1,17 +1,3 @@
-// Copyright (c) 2023 The Gnet Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 //go:build (linux || freebsd || dragonfly || darwin) && gc_opt
 // +build linux freebsd dragonfly darwin
 // +build gc_opt
@@ -21,7 +7,6 @@ package gnet
 import (
 	"net"
 	"testing"
-	"time"
 
 	"golang.org/x/sys/unix"
 
@@ -64,76 +49,65 @@ func testConnMatrix(t *testing.T, n int) {
 	connections.init()
 	el := eventloop{engine: &engine{opts: &Options{}}}
 
+	done := make(chan struct{})
 	go func() {
-		for v := range handleConns {
+		for i := 0; i < n+n/2; i++ {
+			v := <-handleConns
 			switch v.action {
 			case actionAdd:
 				connections.addConn(v.c, 0)
 			case actionDel:
 				connections.delConn(v.c)
-			default:
-				return
 			}
 		}
+		close(done)
 	}()
 
 	pool := goPool.Default()
-	var m int
 	for i := 0; i < n; i++ {
 		c := newTCPConn(i, &el, &unix.SockaddrInet4{}, &net.TCPAddr{}, &net.TCPAddr{})
 		handleConns <- &handleConn{c, actionAdd}
 		if i%2 == 0 {
-			m++
 			pool.Submit(func() {
 				handleConns <- &handleConn{c, actionDel}
 			})
 		}
 	}
+	m := n / 2
 
-	m = n - m
+	<-done
+	if count := connections.loadCount(); count != int32(n)/2 {
+		t.Fatalf("unexpected conn count %d, expected %d", count, int32(n)/2)
+	}
 
-	endTime := time.Now().Add(5 * time.Second)
-
-	for time.Now().Before(endTime) {
-		if connections.loadCount() != int32(n)/2 {
-			time.Sleep(10 * time.Millisecond)
+	for i := 0; i < len(connections.table); i++ {
+		if connections.connCounts[i] == 0 {
 			continue
 		}
 
-		for i := 0; i < len(connections.table); i++ {
-			if connections.connCounts[i] == 0 {
-				continue
+		for j := 0; j < len(connections.table[i]) && m > 0; j++ {
+			m--
+			c := connections.table[i][j]
+			if c == nil {
+				t.Fatalf("unexpected nil connection at row %d, column %d", i, j)
 			}
-
-			for j := 0; j < len(connections.table[i]) && m > 0; j++ {
-				m--
-				c := connections.table[i][j]
-				if c == nil {
-					t.Fatalf("unexpected nil connection at row %d, column %d", i, j)
-				}
-				if c.fd != c.gfd.Fd() {
-					t.Fatalf("unexpected fd %d, expected fd %d", c.gfd.Fd(), c.fd)
-				}
-				if i != c.gfd.ConnMatrixRow() || j != c.gfd.ConnMatrixColumn() {
-					t.Fatalf("unexpected row %d, column %d, expected row %d, column %d",
-						c.gfd.ConnMatrixRow(), c.gfd.ConnMatrixColumn(), i, j)
-				}
-				gfd, ok := connections.fd2gfd[c.fd]
-				if !ok {
-					t.Fatalf("missing gfd for fd %d", c.fd)
-				}
-				if i != gfd.ConnMatrixRow() || j != gfd.ConnMatrixColumn() {
-					t.Fatalf("unexpected row %d, column %d, expected row %d, column %d",
-						gfd.ConnMatrixRow(), gfd.ConnMatrixColumn(), i, j)
-				}
+			if c.fd != c.gfd.Fd() {
+				t.Fatalf("unexpected fd %d, expected fd %d", c.gfd.Fd(), c.fd)
+			}
+			if i != c.gfd.ConnMatrixRow() || j != c.gfd.ConnMatrixColumn() {
+				t.Fatalf("unexpected row %d, column %d, expected row %d, column %d",
+					c.gfd.ConnMatrixRow(), c.gfd.ConnMatrixColumn(), i, j)
+			}
+			gfd, ok := connections.fd2gfd[c.fd]
+			if !ok {
+				t.Fatalf("missing gfd for fd %d", c.fd)
+			}
+			if i != gfd.ConnMatrixRow() || j != gfd.ConnMatrixColumn() {
+				t.Fatalf("unexpected row %d, column %d, expected row %d, column %d",
+					gfd.ConnMatrixRow(), gfd.ConnMatrixColumn(), i, j)
 			}
 		}
-
-		handleConns <- &handleConn{}
-
-		t.Log("connMatrix remains compact after many additions and deletions, test done!")
-		return
 	}
 
-	t.Fatalf("test timeout!")
+	t.Log("connMatrix remains compact after many additions and deletions, test done!")
 }
