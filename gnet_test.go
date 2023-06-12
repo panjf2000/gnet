eca1d0c5ee2e1902e76248a2242a967fc343741f
Andy Pan <panjf2000@gmail.com>
2023-06-12 11:57:48 +0800

    test: fix the hanging issue

--- gnet_test.go
+++ gnet_test.go
@@ -247,6 +247,7 @@ func (s *testServer) OnBoot(eng Engine) (action Action) {
 
 func (s *testServer) OnOpen(c Conn) (out []byte, action Action) {
 	c.SetContext(c)
+	atomic.AddInt32(&s.connected, 1)
 	out = []byte("sweetness\r\n")
 	require.NotNil(s.tester, c.LocalAddr(), "nil local addr")
 	require.NotNil(s.tester, c.RemoteAddr(), "nil remote addr")
