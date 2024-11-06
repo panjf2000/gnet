8e96be77e40176a606c74205346b703e0b022835
zhongweikang <zhongweikang@sfmail.sf-express.com>
2024-11-06 15:24:30 +0800

    bug: fix Conn.Next not safe (#648)

--- connection_unix.go
+++ connection_unix.go
@@ -326,11 +326,10 @@ func (c *conn) Next(n int) (buf []byte, err error) {
 	head, tail := c.inboundBuffer.Peek(n)
 	defer c.inboundBuffer.Discard(n) //nolint:errcheck
 	c.loop.cache.Reset()
+	c.loop.cache.Write(head)
 	if len(head) >= n {
-		c.loop.cache.Write(head)
 		return c.loop.cache.Bytes(), err
 	}
-	c.loop.cache.Write(head)
 	c.loop.cache.Write(tail)
 	if inBufferLen >= n {
 		return c.loop.cache.Bytes(), err
