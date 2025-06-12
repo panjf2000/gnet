8fdeb8d947c876c725a9c32a13426a0ea7bd0d48
qujianping <qujianping@bytedance.com>
2025-06-13 00:00:26 +0800

    opt: skip read socket when read buffer is full

--- eventloop_unix.go
+++ eventloop_unix.go
@@ -256,6 +256,17 @@ func (el *eventloop) read(c *conn) error {
 		return nil
 	}
 
+	if c.inboundBuffer.IsFull() {
+		action := el.eventHandler.OnTraffic(c)
+		switch action {
+		case None:
+		case Close:
+			return el.close(c, nil)
+		case Shutdown:
+			return errorx.ErrEngineShutdown
+		}
+		return nil
+	}
 	var recv int
 	isET := el.engine.opts.EdgeTriggeredIO
 	chunk := el.engine.opts.EdgeTriggeredIOChunk
